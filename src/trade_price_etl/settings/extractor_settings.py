da0d77bda3e90ec96165536bcf69cdc9e0657b17
jintaolong <longjintao59@gmail.com>
2024-01-17 00:25:49 +0800

    refactor: Add slow poll frequencies

--- src/trade_price_etl/settings/extractor_settings.py
+++ src/trade_price_etl/settings/extractor_settings.py
@@ -5,6 +5,8 @@ class ExtractorSettings(BaseSettings):
 
     model_config = SettingsConfigDict(env_prefix='EXTRACTOR_')
 
-    POLL_FREQUENCY_SINGLE_TABLE: int = 5
-    POLL_FREQUENCY_MULTIPLE_TABLES: int = 3
+    POLL_FREQUENCY_SINGLE_TABLE: int = 1
+    POLL_FREQUENCY_MULTIPLE_TABLES: int = 1
+    POLL_FREQUENCY_SINGLE_TABLE_SLOW: int = 5
+    POLL_FREQUENCY_MULTIPLE_TABLES_SLOW: int = 5
     BLOCKADE_STOPPAGE_WAIT: int = 20

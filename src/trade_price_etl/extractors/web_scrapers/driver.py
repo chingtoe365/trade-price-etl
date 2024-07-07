015b9668f42ccab6a774f1ba3e43ce31a553f6c0
jintaolong <longjintao59@gmail.com>
2024-07-07 20:39:00 +0100

    feat: Update available proxy list from public source

--- src/trade_price_etl/extractors/web_scrapers/driver.py
+++ src/trade_price_etl/extractors/web_scrapers/driver.py
@@ -3,6 +3,7 @@ import logging
 import random
 from typing import List
 
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -11,13 +12,31 @@ logger = logging.getLogger(__name__)
 
 
 def get_proxy_list() -> List[str]:
-    return [
+    available_proxy_list = [
         "http://77.68.33.138:8080"
         "http://160.86.242.23:8080",
         "http://172.183.241.1:8080",
         "http://47.251.70.179:80",
         "http://20.235.159.154:80"
     ]
+    proxy_list_resp = requests.get(
+        "https://proxylist.geonode.com/api/proxy-list?protocols=http&limit=500&page=1&sort_by=speed&sort_type=asc",
+        json={}
+    )
+    if proxy_list_resp.status_code == 200:
+        proxy_list_resp_json = proxy_list_resp.json()
+        if "data" in proxy_list_resp_json:
+            proxy_list = proxy_list_resp_json["data"]
+            if len(proxy_list) > 0:
+                # proxy = proxy_list[0]
+                available_proxy_list = [
+                    "http://%s:%s" % (proxy["ip"], str(proxy["port"]))
+                    for proxy in proxy_list
+                ]
+    return available_proxy_list
+
+
+AVAILABLE_PROXIES = get_proxy_list()
 
 
 class SeleniumDriver(ContextDecorator):
@@ -53,7 +72,7 @@ class SeleniumDriver(ContextDecorator):
 
         if not self._first_attempt:
             # randomly select a proxy
-            proxy = random.choice(get_proxy_list())
+            proxy = random.choice(AVAILABLE_PROXIES)
             self._chrome_options.add_argument("--proxy-server=%s" % proxy)
             logger.info("Proxy %s set for selenium" % proxy)
 
